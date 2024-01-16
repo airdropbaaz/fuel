e5fee313918fc4b85ff54e2a798666964b43738b
airdropbaaz <125475579+airdropbaaz@users.noreply.github.com>
2024-01-16 15:19:33 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.3',
     ],
     keywords='dataset data iteration pipeline processing',
     packages=find_packages(exclude=['tests']),
